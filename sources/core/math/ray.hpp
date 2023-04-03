656805a65cadfd1baef25f36c3c82dd1edf2ade7
Oleynikov <Oleynikov.vld@gmail.com>
2023-04-03 22:58:56 +0300

    implemented ray class

---
+++ sources/core/math/ray.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "core/math/vector3.hpp"
+
+template <typename VectorType, typename ArgType>
+class Ray {
+private:
+    VectorType m_Origin;
+    VectorType m_Direction;
+public:
+    Ray(VectorType origin, VectorType direction):
+        m_Origin(origin),
+        m_Direction(direction)
+    {}
+
+    const VectorType &Origin()const{
+        return m_Origin;
+    }
+
+    const VectorType& Direction()const {
+        return m_Direction;
+    }
+
+    VectorType At(ArgType arg)const{
+        return m_Origin + m_Direction * arg;
+    }
+};
+
+using Ray3f = Ray<Vector3f, float>;
