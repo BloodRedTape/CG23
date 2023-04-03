223aa80560f538245c3e5c1b774830c3122c4bb5
Eonus <Alezgora@gmail.com>
2023-04-03 22:58:11 +0300

    add basic types for convinience

---
+++ sources/core/types.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+typedef std::int8_t s8;
+typedef std::uint8_t u8;
+typedef std::int16_t s16;
+typedef std::uint16_t u16;
+typedef std::int32_t s32;
+typedef std::uint32_t u32;
+typedef std::int64_t s64;
+typedef std::uint64_t u64;
+
+typedef u8 byte;
+
+typedef std::size_t size_t;
+
+template <typename T, size_t N>
+constexpr size_t lengthof(T (&)[N]){
+    return N;
+}
