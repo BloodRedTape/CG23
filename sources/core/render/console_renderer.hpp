5932c456606dcc031c7dee8fdc017966f0092df2
BloodRedTape <gefestroyale@gmail.com>
2023-04-18 14:57:29 +0300

    implemented console renderer

---
+++ sources/core/render/console_renderer.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "camera.hpp"
+#include "scene.hpp"
+#include "core/math/ray.hpp"
+#include "core/math/vector2.hpp"
+
+class ConsoleRenderer {
+private:
+	Vector2s m_Viewport;
+public:
+	ConsoleRenderer(Vector2s viewport);
+
+	void Render(const Scene &scene, const Camera &camera);
+
+private:
+
+	Ray3f GenRay(Vector2s pixel_coordinate, const Camera &camera);
+
+	std::optional<HitResult> TraceRay(Ray3f ray, const Scene &scene);
+
+	char Miss();
+
+	char ClosestHit(HitResult hit);
+};
