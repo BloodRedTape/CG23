0d478a9d9759ccf4b1cb07cf2370dc41adc8473d
Oleynikov <Oleynikov.vld@gmail.com>
2023-04-18 14:57:52 +0300

    build simple scene and render it

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,8 +2,21 @@
 
 #include "gtest\gtest.h"
 
+#include "core/render/console_renderer.hpp"
+#include "core/math/sphere.hpp"
+
 int main() {
 	RUN_ALL_TESTS();
 
-	printf("Hello CG23\n");
+
+	Scene scene;
+	scene.Objects.push_back(std::make_unique<Sphere>(Vector3f{0, 0, 0}, 1.f));
+
+	Camera camera{
+		-Vector3f::Forward() * 3.f,
+		70.f
+	};
+	ConsoleRenderer renderer({30, 30});
+
+	renderer.Render(scene, camera);
 }
