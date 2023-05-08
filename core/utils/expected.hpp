64e60a45503fc4587716d52f9c0eb5e787faad25
BloodRedTape <gefestroyale@gmail.com>
2023-05-09 01:59:49 +0300

    fix Expected for movable types

--- core/utils/expected.hpp
+++ core/utils/expected.hpp
@@ -2,18 +2,18 @@
 
 #include "utils/types.hpp"
 
-#include <variant>
 #include <cassert>
+#include <variant>
 
 template<typename ValueType, typename ErrorType>
 class Expected {
     std::variant<ValueType, ErrorType> m_Data;
 public:
     Expected(ValueType value) :
-        m_Data(value) {}
+        m_Data(std::move(value)) {}
 
     Expected(ErrorType error) :
-        m_Data(error) {}
+        m_Data(std::move(error)) {}
 
     bool IsError()const {
         return m_Data.index() == 1;
