70555f537aae676563b52f886be846d73696e5f7
Oleynikov <Oleynikov.vld@gmail.com>
2023-05-10 11:04:56 +0300

    Color now supports linear color representation

--- core/graphics/color.hpp
+++ core/graphics/color.hpp
@@ -2,6 +2,8 @@
 
 #include "utils/types.hpp"
 #include <limits>
+#include "math/vector3.hpp"
+#include "math/functions.hpp"
 
 struct Color {
 	static constexpr u8 MaxChannelValue = std::numeric_limits<u8>::max();
@@ -20,6 +22,12 @@ struct Color {
 		A(alpha)
 	{}
 
+	Color(Vector3f linear_color):
+		R(Math::Clamp<float>(linear_color.x, 0.f, 1.f) * 255),
+		G(Math::Clamp<float>(linear_color.y, 0.f, 1.f) * 255),
+		B(Math::Clamp<float>(linear_color.z, 0.f, 1.f) * 255),
+		A(255)
+	{}
 
 	static const Color Green;
 	static const Color Red;
